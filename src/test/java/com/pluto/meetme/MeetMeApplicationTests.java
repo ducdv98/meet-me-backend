b69557029d4854c1ff6fe843659c31a75d1ec380
ducdv98 <ducdv092@gmail.com>
2024-05-22 14:19:30 +0700

    Initial commit

---
+++ src/test/java/com/pluto/meetme/MeetMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluto.meetme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
